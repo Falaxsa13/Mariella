ef2459fd2732312c98ab36fbb1089e231c48fffa
Alejandro E. <alejandroescobarnunez@gmail.com>
2023-12-24 09:20:29 -0500

    Update ApplicationDbContext and User Models

--- Mariella.Server/Data/ApplicationDbContext.cs
+++ Mariella.Server/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 using Mariella.Server.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Mariella.Server.Data;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext
 {
 	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 	{
--- Mariella.Server/Models/UserModel.cs
+++ Mariella.Server/Models/UserModel.cs
@@ -1,34 +1,19 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace Mariella.Server.Models;
 
 /// <summary>
 /// Represents a user in the database.
 /// </summary>
-public class UserModel
+public class UserModel : IdentityUser
 {
-	[Key]
-	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-	public int Id { get; set; }
-
 	[Required]
 	public required string FirstName { get; set; }
 
 	[Required]
 	public required string LastName { get; set; }
-
-	[Required]
-	public required string Username { get; set; }
-
-	[Required]
-	public required string Email { get; set; }
-
-	[Required]
-	public required string HashedPassword { get; set; }
-
-	[Required]
-	public required string Salt { get; set; }
 
 	[Required]
 	public DateTime CreatedAt { get; set; }
