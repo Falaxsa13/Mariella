a3500b20005681d885f41e9dbb0ecd7fb72019f8
AlejandroEN <alejandroescobarnunez@gmail.com>
2024-02-04 01:00:32 -0500

    Add styled component for ContentSection in Home page

--- mariella.client/src/pages/Home/Home.tsx
+++ mariella.client/src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import Header from "../../common/components/Header/Header";
 import FlexContainer from "../../common/components/FlexContainer/FlexContainer";
 import UserInformation from "./components/UserInformationSection/UserInformationSection";
-import ContentSection from "./components/ContentSection/ContentSection";
+import { ContentSection } from "./Home.Styles";
 import RubricSection from "./components/RubricSection/RubricSection";
 
 const Home = () => {
